e54400e83a140c204f25d544bb2d441b89e05173
sinhtien <sinhtienit@gmail.com>
2021-09-04 19:38:56 -0700

    ghepcode

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
-$(document).ready(function(){
-   
-});
+var srcSpNoiBac = "./Img"
+const anhSpNoiBac = [
+    "bulova.jpg",
+    "chopard.jpg",
+    "frederique.jpg",
+    "gucci.jpg",
+    "hamilton.jpg",
+    "hublot.jpg",
+    "movado.jpg",
+    "omega.jpg",
+    "tissot.jpg",
+    "versace.jpg"
+];
