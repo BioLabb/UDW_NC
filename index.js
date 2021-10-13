897f34bdcdb7fe6518762b115cf143d69d87f56d
Nin 92 <vannin2907lc@gmail.com>
2021-10-13 21:33:56 +0700

    no

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var srcSpNoiBac = "./Img"
-const anhSpNoiBac = [
-    "bulova.jpg",
-    "chopard.jpg",
-    "frederique.jpg",
-    "gucci.jpg",
-    "hamilton.jpg",
-    "hublot.jpg",
-    "movado.jpg",
-    "omega.jpg",
-    "tissot.jpg",
-    "versace.jpg"
-];
+// var srcSpNoiBac = "./Img"
+// const anhSpNoiBac = [
+//     "bulova.jpg",
+//     "chopard.jpg",
+//     "frederique.jpg",
+//     "gucci.jpg",
+//     "hamilton.jpg",
+//     "hublot.jpg",
+//     "movado.jpg",
+//     "omega.jpg",
+//     "tissot.jpg",
+//     "versace.jpg"
+// ];
+console.log("toc em dai ngay hai lan");
