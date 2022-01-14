467ef005ed1c9a3882a4527dd36b57defa7491b9
Nin 92 <vannin2907lc@gmail.com>
2022-01-14 23:23:59 +0700

    storage

--- Layout_1/assets/js/main.js
+++ Layout_1/assets/js/main.js
@@ -5,11 +5,11 @@ var orders = document.querySelectorAll(".content__buy__click");
 var imgs = document.querySelectorAll(".link__img");
 
 function clickOrder () {
-//	alert("Ví của bạn không có tiền. Vui lòng nạp tiền để tiếp tục mua sắm!!");
+	// alert("Ví của bạn không có tiền. Vui lòng nạp tiền để tiếp tục mua sắm!!");
 }
 
 for(var order of orders)
-	order.addEventListener("click", clickOrder());
+	// order.addEventListener("click", clickOrder());
 
 for(var img of imgs)
 	img.addEventListener("click", clickOrder);
---
+++ Layout_1/assets/js/storage.js
@@ -0,0 +1,216 @@
+const product = [
+    {
+        id: 74043,
+        name: "ROLEX CELLINI 50535-0002 MOONPHASE 39",
+        srcImage: "./assets/images/clock/clock4.jpg",
+        realPrice: "185,127,000",
+        sellPrice: "129,589,000"
+    },
+    {
+        id: 84070,
+        name: "Bulova Diamond-Accent Watch 36mm",
+        srcImage: "./assets/images/clock/clock5.jpg",
+        realPrice: "0",
+        sellPrice: "11,260,000"
+    },
+    {
+        id: 62071,
+        name: "Longines Master L2.628.5.77.7 Watch 38.5mm",
+        srcImage: "./assets/images/clock/clock6.jpg",
+        realPrice: "81,695,000",
+        sellPrice: "75,160,000"
+    },
+    {
+        id: 75269,
+        name: "Omega Constellation 123.55.38.21.52.007 Co‑Axial 38mm",
+        srcImage: "./assets/images/clock/clock7.jpg",
+        realPrice: "0",
+        sellPrice: "938,600,000"
+    },
+    {
+        id: 85244,
+        name: "Longines Heritage L2.781.4.13.2 Automatic 41mm",
+        srcImage: "./assets/images/clock/clock10.jpg",
+        realPrice: "80,467,000",
+        sellPrice: "74,029,000"
+    },
+    {
+        id: 70291,
+        name: "Bulova Sutton Automatic Watch 43mm",
+        srcImage: "./assets/images/clock/clock11.jpg",
+        realPrice: "0",
+        sellPrice: "11,260,000"
+    },
+    {
+        id: 90838,
+        name: "Rolex Oyster 126233-0033 Date Just 36mm",
+        srcImage: "./assets/images/clock/clock17.jpg",
+        realPrice: "88,575,000",
+        sellPrice: "82,375,000"
+    },
+    {
+        id: 71342,
+        name: "Versace Dylos Icon Swiss Gold IP Watch 38mm",
+        srcImage: "./assets/images/clock/clock18.jpg",
+        realPrice: "50,755,000",
+        sellPrice: "45,679,000"
+    },
+    {
+        id: 75081,
+        name: "Omega Planet Ocean 600m 215.32.46.51.01.001 Co‑Axial 45.5",
+        srcImage: "./assets/images/clock/clock19.jpg",
+        realPrice: "212,432,000",
+        sellPrice: "201,811,000"
+    },
+    {
+        id: 58914,
+        name: "Tag Heuer Carrera CAR2014.FC6235 Calibre 1887 43",
+        srcImage: "./assets/images/clock/clock20.jpg",
+        realPrice: "0",
+        sellPrice: "122,265,000"
+    },
+    {
+        id: 85386,
+        name: "Swarovski Crystalline Hours Watch 38mm",
+        srcImage: "./assets/images/clock/clock24.jpg",
+        realPrice: "13,310,000",
+        sellPrice: "10,780,000"
+    },
+    {
+        id: 89887,
+        name: "	Bulova Regatta Automatic Silver Watch 40mm",
+        srcImage: "./assets/images/clock/clock25.jpg",
+        realPrice: "0",
+        sellPrice: "15,810,000"
+    },
+    {
+        id: 83260,
+        name: "Versace Palazzo Empire Teal Leather Watch 34mm",
+        srcImage: "./assets/images/clock/clock26.jpg",
+        realPrice: "32,946,000",
+        sellPrice: "28,004,000"
+    },
+    {
+        id: 83251,
+        name: "Versace Glaze Blue Vintage Watch 44mm",
+        srcImage: "./assets/images/clock/clock27.jpg",
+        realPrice: "0",
+        sellPrice: "42,548,000"
+    },
+    {
+        id: 88458,
+        name: "Michael Kors Access Sofie Touchscreen Smartwatch 42mm",
+        srcImage: "./assets/images/clock/clock28.jpg",
+        realPrice: "0",
+        sellPrice: "10,374,000"
+    },
+    {
+        id: 67803,
+        name: "Christian Dior Christal CD144511M001 Automatic Watch 38",
+        srcImage: "./assets/images/clock/clock9.jpg",
+        realPrice: "0",
+        sellPrice: "170,430,000"
+    },
+    {
+        id: 90326,
+        name: "Baume & Mercier Classima 10482 DualTime 42mm",
+        srcImage: "./assets/images/clock/clock29.jpg",
+        realPrice: "0",
+        sellPrice: "62,010,000"
+    },
+    {
+        id: 92605,
+        name: "Versace Medusa Frame Watch 36mm",
+        srcImage: "./assets/images/clock/clock30.jpg",
+        realPrice: "37,476,000",
+        sellPrice: "33,728,000"
+    },
+    {
+        id: 93118,
+        name: "Versace V-Motif Bronze Watch 35mm",
+        srcImage: "./assets/images/clock/clock1.jpg",
+        realPrice: "32,456,000",
+        sellPrice: "29,210,000"
+    },
+    {
+        id: 74626,
+        name: "Rado Centrix Open Heart Automatic Watch 33MM",
+        srcImage: "./assets/images/clock/clock12.jpg",
+        realPrice: "0",
+        sellPrice: "42,998,000"
+    },
+    {
+        id: 67786,
+        name: "Christian Dior Grand Bal CD124BE4A001 Automatic 38",
+        srcImage: "./assets/images/clock/clock13.jpg",
+        realPrice: "871,910,000",
+        sellPrice: "819,595,000"
+    },
+    {
+        id: 55288,
+        name: "Movado Concerto White Mother-Of-Pearl Round Watch 30mm",
+        srcImage: "./assets/images/clock/clock14.jpg",
+        realPrice: "61,627,000",
+        sellPrice: "54,848,000"
+    },
+    {
+        id: 74089,
+        name: "Omega De Ville Prestige 424.22.33.20.55.001 Watch 32.7mm",
+        srcImage: "./assets/images/clock/clock15.jpg",
+        realPrice: "161,785,000",
+        sellPrice: "156,931,000"
+    },
+    {
+        id: 70721,
+        name: "Tissot Bella Ora T103.310.36.111.00 Watch 31mm",
+        srcImage: "./assets/images/clock/clock16.jpg",
+        realPrice: "9,653,000",
+        sellPrice: "8,687,000"
+    },
+    {
+        id: 63304,
+        name: "Citizen L Sunrise Eco-Drive Watch 30mm",
+        srcImage: "./assets/images/clock/clock21.jpg",
+        realPrice: "9,757,000",
+        sellPrice: "8,293,000"
+    },
+    {
+        id: 7459024626,
+        name: "Chopard Happy Diamonds 209341-5001 18-Karat 29.40mm",
+        srcImage: "./assets/images/clock/clock22.jpg",
+        realPrice: "883,912,000",
+        sellPrice: "866,233,000"
+    },
+    {
+        id: 75048,
+        name: "Michael Kors Portia Watch 36.5mm",
+        srcImage: "./assets/images/clock/clock23.jpg",
+        realPrice: "8,336,000",
+        sellPrice: "7,919,000"
+    },
+    {
+        id: 86816,
+        name: "Swarovski Octea Lux Chrono Watch 39mm",
+        srcImage: "./assets/images/clock/clock2.jpg",
+        realPrice: "14,560,000",
+        sellPrice: "13,100,000"
+    },
+    {
+        id: 89017,
+        name: "Chopard Happy 278582-3005 Sport Round Watch 36mm",
+        srcImage: "./assets/images/clock/clock3.jpg",
+        realPrice: "0",
+        sellPrice: "150,696,000"
+    },
+    {
+        id: 85044,
+        name: "Versace V-Motif Vintage Logo Watch 35mm",
+        srcImage: "./assets/images/clock/clock8.jpg",
+        realPrice: "0",
+        sellPrice: "81,284,000"
+    }
+]
+
+const newArr = product.find(item => item.id === 67803)
+
+console.log(product)
