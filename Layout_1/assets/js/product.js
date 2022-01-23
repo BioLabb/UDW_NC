326d0f7d8c453c5a6a4570f2f0c48c992c3d108d
sinhtien <sinhtienit@gmail.com>
2022-01-23 16:09:12 +0700

    tien

--- Layout_1/assets/js/product.js
+++ Layout_1/assets/js/product.js
@@ -2,19 +2,18 @@ import {createItem, setListItem, getListItem} from './item.js';
 
 var nameHeading = document.querySelector(".control__space");
 var nameClock = document.querySelector(".product__buy-heading");  // tên đồng hồ
+var id = document.querySelector(".product__buy-id");
 var sell = document.querySelector(".product__buy-price-old"); // tiền
 var real = document.querySelector(".product__buy-price-sale"); // tạm tính khi chưa ap dụng mã giảm giá
 var imgSmall = document.querySelector(".product-container__primary-image-small img");
 var imgBig = document.querySelector(".product-container__primary-image-big img")
 
 
-console.log(nameHeading);
 var infoCloks = getListItem("productItem");
-console.log(infoCloks);
+id.innerText = infoCloks.id;
 nameHeading.innerText = infoCloks.name;
 nameClock.innerText = infoCloks.name;
 imgSmall.setAttribute("src",infoCloks.img);
-console.log(infoCloks.img)
 imgBig.setAttribute("src",infoCloks.img);
 
 if(infoCloks.sell)
